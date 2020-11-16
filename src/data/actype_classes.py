c6d77dec3007bccc7084581fa9b4ed0078b7d4af
Will Parr <parr.will.08@gmail.com>
2020-11-16 16:06:06 +0000

    Added class_id and updated class regex

--- src/data/actype_classes.py
+++ src/data/actype_classes.py
@@ -1,25 +1,98 @@
 ACTYPE_DICT = {
-    "Boeing 707": r"Boeing 707-",
-    "Boeing 717": r"Boeing 717-",
-    "Boeing 727": r"Boeing 727-",
-    "Boeing 737": r"Boeing 737-",
-    "Boeing 747": r"Boeing 747-",
-    "Boeing 757": r"Boeing 757-",
-    "Boeing 767": r"Boeing 767-",
-    "Boeing 777": r"Boeing 777-",
-    "Boeing 787": r"Boeing 787-",
-    "ATR 42": r"ATR ATR-42-",
-    "ATR 72": r"ATR ATR-72-",
-    "Airbus A220": r"Airbus A220|Bombardier CSeries CS",
-    "Airbus A300": r"Airbus A300",
-    "Airbus A310": r"Airbus A310",
-    "Airbus A320 Family": r"Airbus A3[12][8901]",
-    "Airbus A330": r"Airbus A330",
-    "Airbus A340": r"Airbus A340",
-    "Airbus A350": r"Airbus A350",
-    "Airbus A380": r"Airbus A380",
-    "Bombardier DHC-8-400": r"Bombardier DHC-8-40[12] Q400",
-    "Bombardier CRJ Family": r"Bombardier CRJ-[1279]",
-    "Embraer ERJ": r"Embraer ERJ-1[34][05]",
-    "Embraer E-Jet": r"Embraer 1[7-9][05]",
+    "Boeing 707": {
+        "class_id": 0,
+		"match_regex": r"Boeing 707-",
+	},
+    "Boeing 717": {
+        "class_id": 1,
+		"match_regex": r"Boeing 717-",
+	},
+    "Boeing 727": {
+        "class_id": 2,
+		"match_regex": r"Boeing 727-",
+	},
+    "Boeing 737": {
+        "class_id": 3,
+		"match_regex": r"Boeing 737-",
+	},
+    "Boeing 747": {
+        "class_id": 4,
+		"match_regex": r"Boeing 747-",
+	},
+    "Boeing 757": {
+        "class_id": 5,
+		"match_regex": r"Boeing 757-",
+	},
+    "Boeing 767": {
+        "class_id": 6,
+		"match_regex": r"Boeing 767-",
+	},
+    "Boeing 777": {
+        "class_id": 7,
+		"match_regex": r"Boeing 777-",
+	},
+    "Boeing 787": {
+        "class_id": 8,
+		"match_regex": r"Boeing 787-",
+	},
+    "ATR 42": {
+        "class_id": 9,
+		"match_regex": r"ATR ATR-42-",
+	},
+    "ATR 72": {
+        "class_id": 10,
+		"match_regex": r"ATR ATR-72-",
+	},
+    "Airbus A220": {
+        "class_id": 11,
+		"match_regex": r"Airbus A220|Bombardier CSeries CS",
+	},
+    "Airbus A300": {
+        "class_id": 12,
+		"match_regex": r"Airbus A300",
+	},
+    "Airbus A310": {
+        "class_id": 13,
+		"match_regex": r"Airbus A310",
+	},
+    "Airbus A320 Family": {
+        "class_id": 14,
+		"match_regex": r"Airbus A3[12][8901]",
+	},
+    "Airbus A330": {
+        "class_id": 15,
+		"match_regex": r"Airbus A330",
+	},
+    "Airbus A340": {
+        "class_id": 16,
+		"match_regex": r"Airbus A340",
+	},
+    "Airbus A350": {
+        "class_id": 17,
+		"match_regex": r"Airbus A350",
+	},
+    "Airbus A380": {
+        "class_id": 18,
+		"match_regex": r"Airbus A380",
+	},
+    "Bombardier DHC-8-400": {
+        "class_id": 19,
+		"match_regex": r"Bombardier DHC-8-40[12] Q400",
+	},
+    "Bombardier CRJ 200": {
+        "class_id": 20,
+		"match_regex": r"Bombardier CRJ-[12]00\D",
+	},
+    "Bombardier CRJ 700": {
+        "class_id": 21,
+		"match_regex": r"Bombardier CRJ-([79]0[0-9]|1000)",
+	},
+    "Embraer ERJ": {
+        "class_id": 22,
+		"match_regex": r"Embraer ERJ-1[34][05]",
+	},
+    "Embraer E-Jet": {
+        "class_id": 23,
+		"match_regex": r"Embraer 1[7-9][05]",
+	},
 }
