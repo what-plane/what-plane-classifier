2ea3e734413428f51d163a090a9983846b4d927a
Will Parr <parr.will.08@gmail.com>
2020-11-19 11:40:17 +0000

    Refactor data_helpers

--- src/models/data_helpers.py
+++ src/models/data_helpers.py
@@ -1,24 +1,39 @@
 from pathlib import Path
 
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
 
-def load_data(data_dir):
+def load_data(data_dir, batch_size):
+    # TODO: Write docstring
+    """[summary]
 
+    Args:
+        data_dir ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
     data_dir = Path(data_dir)
     train_dir = data_dir / "train"
     test_dir = data_dir / "test"
     valid_dir = data_dir / "valid"
 
+    NORM = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+
     data_transforms = {
         "train": transforms.Compose(
             [
                 transforms.Resize(240),
+                transforms.RandomRotation(10),
                 transforms.RandomResizedCrop(224),
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+                transforms.Normalize(*NORM),
             ]
         ),
         "valid": transforms.Compose(
@@ -26,7 +41,7 @@ def load_data(data_dir):
                 transforms.Resize(256),
                 transforms.CenterCrop(224),
                 transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+                transforms.Normalize(*NORM),
             ]
         ),
         "test": transforms.Compose(
@@ -34,7 +49,7 @@ def load_data(data_dir):
                 transforms.Resize(256),
                 transforms.CenterCrop(224),
                 transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+                transforms.Normalize(*NORM),
             ]
         ),
     }
@@ -45,43 +60,53 @@ def load_data(data_dir):
     }
 
     dataloaders = {
-        x: DataLoader(image_datasets[x], batch_size=10, shuffle=True, num_workers=4)
+        x: DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=4)
         for x in ["train", "valid", "test"]
     }
 
     dataset_sizes = {x: len(image_datasets[x]) for x in ["train", "test", "valid"]}
 
     class_names = image_datasets["train"].classes
-    print(dataloaders)
-    print(dataset_sizes)
-    print(image_datasets)
+
     return dataloaders, dataset_sizes, image_datasets
 
+
 def process_image(image_path):
-    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+    """Scales, crops, and normalizes a PIL image for a PyTorch model,
         returns an Numpy array
-    '''
+
+    Args:
+        image_path ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
     img_pil = Image.open(image_path)
-   
-    adjustments = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
-    
+
+    adjustments = transforms.Compose(
+        [
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
+
     img_tensor = adjustments(img_pil)
-    
+
     return img_tensor
 
-def imshow(inp, title=None):
+
+def imshow(img_tensor, title=None):
     """Imshow for Tensor."""
-    inp = inp.numpy().transpose((1, 2, 0))
+    img_tensor = img_tensor.numpy().transpose((1, 2, 0))
     mean = np.array([0.485, 0.456, 0.406])
     std = np.array([0.229, 0.224, 0.225])
-    inp = std * inp + mean
-    inp = np.clip(inp, 0, 1)
-    plt.imshow(inp)
+    img_tensor = std * img_tensor + mean
+    img_tensor = np.clip(img_tensor, 0, 1)
+    plt.imshow(img_tensor)
     if title is not None:
         plt.title(title)
     plt.pause(0.001)  # pause a bit so that plots are updated
+
+    return
