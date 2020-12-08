612274877df4b2c5d8c927749770b3d5ba8b5baf
Ashrith Maudgalya <ashrith.maudgalya@gmail.com>
2020-12-08 19:00:58 +0000

    Updated index.html in line with app.py changes

--- app/app.py
+++ app/app.py
@@ -28,7 +28,7 @@ with open("./data/imagenet_class_index.json") as f:
     imagenet_class_index = json.load(f)
 
 imagenet_model = mh.initialize_model(
-    "densenet161", imagenet_class_index.values(), replace_classifier=False)
+    "densenet161", [item[1] for item in list(imagenet_class_index.values())], replace_classifier=False)
 imagenet_model.eval()
 
 
@@ -40,23 +40,6 @@ def load_inference_model(model_path):
 def transform_image(image_bytes):
     image = Image.open(io.BytesIO(image_bytes))
     return PREDICT_TRANSFORM(image).unsqueeze(0)
-
-
-def imagenet_pred(image_bytes):
-    tensor = transform_image(image_bytes=image_bytes)
-    outputs = imagenet_model.forward(tensor)
-    _, y_hat = outputs.max(1)
-    percents = torch.nn.functional.softmax(outputs, dim=1)
-    # top5_vals, top5_inds = percents.topk(5, sorted=True)
-    # print(top5_vals[0], top5_inds[0])
-    predicted_idx = str(y_hat.item())
-    print(predicted_idx)
-    return imagenet_class_index[predicted_idx]
-
-
-def imagenet_pred_2(file_path, model):
-    top5_vals, top5_class = predict(file_path, model, topk=5)
-    return top5_vals, top5_class
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -72,16 +55,17 @@ def image_predict():
         buf = BytesIO()
         img.save(buf, 'jpeg')
         buf.seek(0)
-        img_bytes = buf.read()
         buf.close()
         # Load ImageNet model to check image type
-        class_id, class_name = imagenet_pred_2(file, imagenet_model)
+        class_ids, class_names = predict(file, imagenet_model, topk=5)
+
+        print("imagenet:", class_ids, class_names)
         # If image is an airliner, load inference model
-        if class_name != 'airliner':
-            return render_template('index.html', class_name=class_name, class_id=class_id, filename=filename)
+        if 'airliner' not in class_names:
+            return render_template('index.html', class_name=class_names[0], class_id=str(class_ids[0]), filename=filename)
         else:
             model = load_inference_model(
-                '../models\model_airliners_net_balanced_densenet161_SGD.pth')
+                '../models/model_airliners_net_balanced_densenet161_SGD.pth')
             top_probs, top_classes = predict(file, model, topk=1)
             return render_template('index.html', class_name=top_classes[0], class_id=round(top_probs[0]*100, 1), filename=filename)
 
