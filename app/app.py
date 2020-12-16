30995fec047b261442c5b4dfdef784483dc52684
Ashrith Maudgalya <ashrith.maudgalya@gmail.com>
2020-12-16 17:01:33 +0000

    Changed Flask to serve model as API

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, jsonify, redirect
+from flask import Flask, request, jsonify
 from PIL import Image
 from werkzeug.utils import secure_filename
 import torchvision.transforms as transforms
@@ -19,10 +19,7 @@ from src.models.predict_model import test, predict  # noqa
 import src.models.visualise_helpers as vh  # noqa
 
 
-UPLOAD_FOLDER = './static/uploads'
-
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 with open("./data/imagenet_class_index.json") as f:
     imagenet_class_index = json.load(f)
@@ -42,34 +39,19 @@ def transform_image(image_bytes):
     return PREDICT_TRANSFORM(image).unsqueeze(0)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/predict', methods=['GET', 'POST'])
 def image_predict():
     if request.method == 'POST':
-        if 'file' not in request.files:
-            return redirect(request.url)
         file = request.files['file']
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        img = Image.open(os.path.join(
-            app.config['UPLOAD_FOLDER'], filename))
-        buf = BytesIO()
-        img.save(buf, 'jpeg')
-        buf.seek(0)
-        buf.close()
-        # Load ImageNet model to check image type
         class_ids, class_names = predict(file, imagenet_model, topk=5)
-
-        print("imagenet:", class_ids, class_names)
         # If image is an airliner, load inference model
         if 'airliner' not in class_names:
-            return render_template('index.html', class_name=class_names[0], class_id=str(class_ids[0]), filename=filename)
+            return jsonify({'class_name': class_names[0], 'class_id': str(class_ids[0])})
         else:
             model = load_inference_model(
                 '../models/model_ash_densenet161_SGD.pth')
             top_probs, top_classes = predict(file, model, topk=1)
-            return render_template('index.html', class_name=top_classes[0], class_id=round(top_probs[0]*100, 1), filename=filename)
-
-    return render_template('index.html')
+            return jsonify({'class_name': top_classes[0], 'class_pred': round(top_probs[0]*100, 1)})
 
 
 if __name__ == '__main__':
--- app/send_reqs.py
+++ app/send_reqs.py
@@ -1,6 +1,6 @@
 import requests
 
 resp = requests.post("http://localhost:5000/predict",
-                     files={"file": open('a350.jpeg', 'rb')})
+                     files={"file": open('a350_1.jpg', 'rb')})
 
 print(resp.json())
