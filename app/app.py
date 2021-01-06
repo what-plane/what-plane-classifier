4f0e47c278f0fc89121cec80893e04ba13d71c33
Will Parr <parr.will.08@gmail.com>
2021-01-06 23:09:56 +0000

    Working FastAPI implementation

--- app/app.py
+++ app/app.py
@@ -1,10 +1,13 @@
 import os
 import io
 import sys
-from pathlib import Path
+from pathlib import Path as PyPath
 import json
+from typing import List
 
-from flask import Flask, jsonify, abort
+import uvicorn
+from fastapi import FastAPI, Path, Query, HTTPException
+from pydantic import BaseModel
 from PIL import Image
 from azure.storage.blob import BlobServiceClient
 import torch
@@ -15,9 +18,7 @@ import whatplane.models.model_helpers as mh
 from whatplane.models.data_helpers import PREDICT_TRANSFORM
 from whatplane.models.predict_model import predict_image_data
 
-app = Flask(__name__)
-
-CWD = Path(".")
+CWD = PyPath(".")
 IMAGE_UPLOAD_CONTAINTER = "uploaded-images"
 CLASSIFIED_IMAGE_CONTAINER = "uploaded-images-airliners"
 CONNECT_STR = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
@@ -37,55 +38,65 @@ imagenet_model = mh.initialize_model(
 )
 whatplane_model = mh.load_model(CWD / "../models/model.pth")
 
+app = FastAPI(
+    title="WhatPlane", description="Recognising Aircraft with Deep Learning", version="0.3.0"
+)
+
+
+class Prediction(BaseModel):
+    class_name: str = "Boeing 757"
+    class_prob: float = 0.92
+
+
+class PredictionSet(BaseModel):
+    predictions: List[Prediction]
+    topk: int = 1
+    predictor: str = "whatplane"
+
 
 def prepare_response(probs, class_names, predictor):
     predictions = [
-        {"class_name": class_name, "class_prob": round(probs[i], 3)}
+        Prediction(class_name=class_name, class_prob=round(probs[i], 3))
         for i, class_name in enumerate(class_names)
     ]
-    response = TEMPLATE_RESPONSE.copy()
-    response["data"]["predictions"] = predictions
-    response["data"]["topk"] = len(predictions)
-    response["data"]["predictor"] = predictor
-    return response
+    return PredictionSet(predictions=predictions, topk=len(predictions), predictor=predictor)
 
 
-@app.errorhandler(404)
-def resource_not_found(e):
-    return jsonify(error=str(e)), 404
-
-
-@app.route("/predict/<string:filename>", methods=["GET"])
-def image_predict_api(filename):
-
-    if len(filename) == 0:
-        abort(404, description="Resource not found")
+@app.get("/predict/{filename}", response_model=PredictionSet, status_code=200, tags=["predict"])
+async def image_prediction_api(
+    filename: str = Path(
+        ..., title="The filename of the image uploaded by the frontend application", min_length=36
+    ),
+    topk: int = Query(1, title="The number of classes returned ordered by probability", ge=1, le=5),
+):
 
     uploaded_blob = blob_service_client.get_blob_client(
         container=IMAGE_UPLOAD_CONTAINTER, blob=filename
     )
 
     if not uploaded_blob.exists():
-        abort(404, description="Resource not found")
+        raise HTTPException(status_code=404, detail="Resource not found")
 
     content_type = uploaded_blob.get_blob_properties()["content_settings"]["content_type"]
 
     if content_type not in ["image/jpeg", "image/png"]:
-        abort(404, description=f"File type {content_type} not supported")
+        raise HTTPException(status_code=404, detail=f"File type {content_type} not supported")
 
     image = Image.open(io.BytesIO(uploaded_blob.download_blob().readall()))
 
     imagenet_probs, imagenet_classes = predict_image_data(image, imagenet_model, topk=5)
 
     # Take only classes with prob > 0.5
-    imagenet_likely_class = [imagenet_classes[i] for i, prob in enumerate(imagenet_probs) if prob > 0.5]
+    imagenet_likely_class = [
+        imagenet_classes[i] for i, prob in enumerate(imagenet_probs) if prob > 0.5
+    ]
 
     # If image is an airliner, load inference model
     if "airliner" not in imagenet_likely_class:
-        result = jsonify(prepare_response([imagenet_probs[0]], [imagenet_classes[0]], "imagenet"))
+        response = prepare_response(imagenet_probs[:topk], imagenet_classes[:topk], "imagenet")
     else:
-        whatplane_probs, whatplane_classes = predict_image_data(image, whatplane_model, topk=1)
-        result = jsonify(prepare_response(whatplane_probs, whatplane_classes, "whatplane"))
+        whatplane_probs, whatplane_classes = predict_image_data(image, whatplane_model, topk=topk)
+        response = prepare_response(whatplane_probs, whatplane_classes, "whatplane")
 
         # Transfer blob to classified image container
         airliner_blob = blob_service_client.get_blob_client(
@@ -93,8 +104,8 @@ def image_predict_api(filename):
         )
         airliner_blob.start_copy_from_url(uploaded_blob.url)
 
-    return result
+    return response
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
