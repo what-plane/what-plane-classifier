744ccb632d60330381962132d35a686da9d6ee2f
Will Parr <parr.will.08@gmail.com>
2021-02-04 18:53:06 +0000

    Add CORS support to API

--- api/main.py
+++ api/main.py
@@ -1,10 +1,15 @@
 import uvicorn
 
-from fastapi import FastAPI, Path, Query, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import predict, health
 
+ORIGINS = [
+    "http://localhost:3000"
+    ]
+
+ORIGINS_REGEX = r"https://.*whatplaneis\.it"
 
 app = FastAPI(
     title="WhatPlane", description="Recognising Aircraft with Deep Learning", version="0.3.1"
@@ -12,3 +17,13 @@ app = FastAPI(
 
 app.include_router(predict.router)
 app.include_router(health.router)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ORIGINS,
+    allow_origin_regex=ORIGINS_REGEX,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
