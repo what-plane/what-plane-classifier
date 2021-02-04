ddea617ab051c5b3dc83daef93d986ace5e14dd4
Will Parr <parr.will.08@gmail.com>
2021-02-04 00:26:44 +0000

    Add UUID validation to /predict API

--- api/dependancies/blob.py
+++ api/dependancies/blob.py
@@ -1,5 +1,6 @@
 import os
 import io
+from uuid import UUID
 
 from fastapi import HTTPException
 from PIL import Image
@@ -11,12 +12,47 @@ CONNECT_STR = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 blob_service_client = BlobServiceClient.from_connection_string(CONNECT_STR)
 
 
+def is_valid_uuid(uuid_to_test, version=4):
+    """
+    Check if uuid_to_test is a valid UUID.
+
+     Parameters
+    ----------
+    uuid_to_test : str
+    version : {1, 2, 3, 4}
+
+     Returns
+    -------
+    `True` if uuid_to_test is a valid UUID, otherwise `False`.
+
+     Examples
+    --------
+    >>> is_valid_uuid('c9bf9e57-1685-4c89-bafb-ff5af830be8a')
+    True
+    >>> is_valid_uuid('c9bf9e58')
+    False
+    """
+
+    try:
+        uuid_obj = UUID(uuid_to_test, version=version)
+    except ValueError:
+        return False
+    return str(uuid_obj) == uuid_to_test
+
+
 class ImageBlobClient:
     def __init__(self, uuid):
-        self.UUID = uuid
-        self.uploaded_blob = self._return_uploaded_blob()
+        self.UUID = self._validate_uuid(uuid)
+        self.uploaded_blob = self._validate_uploaded_blob()
 
-    def _return_uploaded_blob(self):
+    @staticmethod
+    def _validate_uuid(uuid):
+        if not is_valid_uuid(uuid):
+            raise HTTPException(status_code=400, detail="Invalid UUID")
+
+        return uuid
+
+    def _validate_uploaded_blob(self):
         uploaded_blob = blob_service_client.get_blob_client(
             container=IMAGE_UPLOAD_CONTAINTER, blob=self.UUID
         )
