fc2e884b4bb22e47238d899f6102da29da4e349f
Will Parr <parr.will.08@gmail.com>
2021-02-10 22:46:18 +0000

    Added docstring for health api

--- api/routers/health.py
+++ api/routers/health.py
@@ -15,5 +15,10 @@ class HealthSet(BaseModel):
     "/health", response_model=HealthSet, status_code=200, tags=["health"],
 )
 async def health_check() -> HealthSet:
+    """API to check status and model version
+
+    Returns:
+        HealthSet: Model version and a ping
+    """
     model_version = getattr(model.whatplane_model, "version", "Not Set")
     return HealthSet(model_version=model_version, ping="Pong!")
