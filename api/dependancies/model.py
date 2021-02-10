d8dd82092ee6f14499d21e48c3947442ed1856e4
Ashrith Yerrapragada <ashrith.maudgalya@gmail.com>
2021-02-10 12:10:30 +0000

    Added function to return WhatPlane classes

--- api/dependancies/model.py
+++ api/dependancies/model.py
@@ -15,9 +15,7 @@ with open(BASE_DIR / "api/imagenet_class_index.json") as f:
 # Set PyTorch cache directory to where the model is stored
 hub.set_dir(str(MODELS_DIR.resolve()))
 imagenet_model = mh.initialize_model(
-    "densenet161",
-    [item[1] for item in list(imagenet_class_index.values())],
-    replace_classifier=False,
+    "densenet161", [item[1] for item in list(imagenet_class_index.values())], replace_classifier=False,
 )
 whatplane_model = mh.load_model(MODELS_DIR / "model.pth")
 
@@ -27,11 +25,13 @@ def predict_imagenet(image, topk):
     imagenet_probs, imagenet_classes = predict_image_data(image, imagenet_model, topk)
 
     # Take only classes with prob > 0.5
-    imagenet_likely_class = [
-        imagenet_classes[i] for i, prob in enumerate(imagenet_probs) if prob > 0.5
-    ]
+    imagenet_likely_class = [imagenet_classes[i] for i, prob in enumerate(imagenet_probs) if prob > 0.5]
     return imagenet_probs, imagenet_classes, imagenet_likely_class
 
 
 def predict_whatplane(image, topk):
     return predict_image_data(image, whatplane_model, topk)
+
+
+def get_wp_classes():
+    return whatplane_model.class_names
