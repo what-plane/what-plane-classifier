bb9717ff1fde478fcca3a7aa730895fd06b0e7d3
Ashrith Yerrapragada <ashrith.maudgalya@gmail.com>
2021-02-10 23:04:14 +0000

    Added docstrings for model.py

--- api/dependancies/model.py
+++ api/dependancies/model.py
@@ -17,18 +17,24 @@ with open(BASE_DIR / "api/imagenet_class_index.json") as f:
 # Set PyTorch cache directory to where the model is stored
 hub.set_dir(str(MODELS_DIR.resolve()))
 imagenet_model = mh.initialize_model(
-    "densenet161",
-    [item[1] for item in list(imagenet_class_index.values())],
-    replace_classifier=False,
+    "densenet161", [item[1] for item in list(imagenet_class_index.values())], replace_classifier=False,
 )
 whatplane_model = mh.load_model(MODELS_DIR / "model.pth")
 
+
 def should_predict_whatplane(imagenet_probs: List[float], imagenet_classes: List[str]) -> bool:
+    """Function to check which model to use for prediction. 
+
+    Args:
+        imagenet_probs (List[float]): List of probabilities returned from ImageNet
+        imagenet_classes (List[str]): List of class names returned from ImageNetwork
+
+    Returns:
+        bool: Returns True if WhatPlane is to be used, otherwise returns False
+    """
     VALID_CLASSES = ["Airliner", "Wing"]
 
-    imagenet_likely_class = set(
-        [imagenet_classes[i] for i, prob in enumerate(imagenet_probs) if prob > 0.5]
-    )
+    imagenet_likely_class = set([imagenet_classes[i] for i, prob in enumerate(imagenet_probs) if prob > 0.5])
 
     if imagenet_classes[0] in VALID_CLASSES:
         return True
@@ -37,5 +43,11 @@ def should_predict_whatplane(imagenet_probs: List[float], imagenet_classes: List
 
     return False
 
+
 def get_wp_classes() -> List:
+    """Function to extract the the list of classes from whatplane
+
+    Returns:
+        List: List of all classes from WhatPlane
+    """
     return whatplane_model.class_names
